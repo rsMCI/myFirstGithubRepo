20586be822334857fbd8de91ff43de5e09c1e99c
Rudolf Stoffner <rudolf.stoffner@mci.edu>
2026-04-20 16:16:28 +0200

    Correct capitalization in main entry point message

--- some_code.py
+++ some_code.py
@@ -3,5 +3,5 @@ def main():
     print("Working from the clouds")
 
 if __name__ == "__main__":
-    print("This is the main entry point of the program.")
+    print("This is the main ENTRY point of the program.")
     main()
