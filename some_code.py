8bf011cf296f7a38b5c10795b30c682cce06765a
Rudolf Stoffner <rudolf.stoffner@mci.edu>
2026-04-21 07:42:33 +0200

    Remove two print statements from main function

--- some_code.py
+++ some_code.py
@@ -1,7 +1,6 @@
 def main():
     print("Hello, World!")
-    print("Working from the clouds")
-    print("This is a simple Python program.")
+    print("Working from the clou
 
 if __name__ == "__main__":
     print("This is the main ENTRY point of the program.")
