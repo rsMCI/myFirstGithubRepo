714c1ee15f4b8450e0e220f0f0a6d2e20e6353b3
Rudolf Stoffner <rudolf.stoffner@mci.edu>
2026-04-21 07:43:47 +0200

    Fix typo in print statement

--- some_code.py
+++ some_code.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello, World!")
-    print("Working from the clou
+    print("Working from the clouds")
 
 if __name__ == "__main__":
     print("This is the main ENTRY point of the program.")
